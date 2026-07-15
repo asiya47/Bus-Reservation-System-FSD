422e02fd470dce8bb4b1fc0068f0d0e467236f35
asiya md <asiyabenazeer@gmail.com>
2026-07-15 19:12:07 +0530

    Update api.js

--- Bus-Reserve/client/src/services/api.js
+++ Bus-Reserve/client/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // Use localhost when running locally
+  // baseURL: "http://localhost:5000/api",
+
+  // Use Render when deployed
+  baseURL: "https://bus-reservation-api.onrender.com/api",
 });
 
 export default API;
